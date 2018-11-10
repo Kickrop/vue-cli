3822923d8b552931770f928414db33022b8af312
den <kickrop@gmail.com>
2018-11-11 01:20:37 +0300

    file structure

--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 // import Home from './Home.vue'
 // // using global components
-
+  // import Header from './components/Header.vue'
+  // import ListGroup from './components/ListGroup.vue'
+  // import ServerDetails from './components/ServerDetails.vue'
+  // import Footer from './components/Footer.vue'
+  // Vue.component('app-header', Header);
+  // Vue.component('app-list-group', ListGroup);
+  // Vue.component('app-server-details', ServerDetails);
+  // Vue.component('app-footer', Footer);
 // Vue.component('app-servers', Home);
 
 new Vue({
